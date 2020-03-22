137d296c9a7f15d19c39a4f0306b592b55b1c9f5
Iñigo <ik012108bwk@plaiaundi.net>
2020-03-22 19:42:28 +0100

    done

--- mi proyecto/Program.vb
+++ mi proyecto/Program.vb
@@ -2,6 +2,21 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("introduce el numero de numeros de fibonacci que quieres  mostrar")
+        Dim x As Integer = Console.ReadLine()
+
+        For y As Integer = 0 To x - 1
+            Console.Write(Fibo(y))
+            Console.Write(", ")
+        Next
+
     End Sub
+    Public Function Fibo(ByVal j As Integer) As Integer
+        If (j = 0 Or j = 1) Then
+            Return j
+        Else
+            Return Fibo(j - 1) + Fibo(j - 2)
+        End If
+    End Function
+
 End Module
